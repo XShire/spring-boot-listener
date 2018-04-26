5bb047912e0cc4a1ee29066950c3bd1d7b185edb
liucz <liu227world@163.com>
2018-04-26 11:07:52 +0800

    改为注解方式实现监听

--- src/main/java/com/liucz/listener/MyListener.java
+++ src/main/java/com/liucz/listener/MyListener.java
@@ -1,21 +1,21 @@
 package com.liucz.listener;
 
 import com.liucz.event.MyEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 /**
  * 测试用自定义监听器,监听事件为MyEvent
  */
 @Component
-public class MyListener implements ApplicationListener<MyEvent> {
+public class MyListener {
 
     /**
      * 对监听到的事件进行处理
      * @param myEvent
      */
-    @Override
-    public void onApplicationEvent(MyEvent myEvent) {
+    @EventListener
+    public void point(MyEvent myEvent) {
 
         /*
           这里不做处理，只对消息进行透传打印，实际情况，
